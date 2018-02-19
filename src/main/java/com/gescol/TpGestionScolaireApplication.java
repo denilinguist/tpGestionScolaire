ffd366fdb4eb4d89be950921e06003c3fe819099
denilinguist <joel.eline@gmail.com>
2018-02-19 10:49:22 +0100

    initial commit

---
+++ src/main/java/com/gescol/TpGestionScolaireApplication.java
@@ -0,0 +1,12 @@
+package com.gescol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGestionScolaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpGestionScolaireApplication.class, args);
+	}
+}
